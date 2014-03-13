87250e8e7e9e4902071c8a481940dd7684261e0d
tjchester <tjchester@me.com>
2014-03-13 13:33:32 -0400

    Initial Commit

---
+++ Select Random Items/Select_Random_Items.h
@@ -0,0 +1,15 @@
+//
+//  Select_Random_Items.h
+//  Select Random Items
+//
+//  Created by Thomas Chester on 3/13/14.
+//  Copyright (c) 2014 Thomas Chester. All rights reserved.
+//
+
+#import <Automator/AMBundleAction.h>
+
+@interface Select_Random_Items : AMBundleAction
+
+- (id)runWithInput:(id)input fromAction:(AMAction *)anAction error:(NSDictionary **)errorInfo;
+
+@end
---
+++ Select Random Items/Select_Random_Items.m
@@ -0,0 +1,20 @@
+//
+//  Select_Random_Items.m
+//  Select Random Items
+//
+//  Created by Thomas Chester on 3/13/14.
+//  Copyright (c) 2014 Thomas Chester. All rights reserved.
+//
+
+#import "Select_Random_Items.h"
+
+@implementation Select_Random_Items
+
+- (id)runWithInput:(id)input fromAction:(AMAction *)anAction error:(NSDictionary **)errorInfo
+{
+	// Add your code here, returning the data to be passed to the next action.
+	
+	return input;
+}
+
+@end
